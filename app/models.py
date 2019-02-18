781b249e80f2e857d8c2648c048695163ee7d218
wanjikuciku <lorna.wanjiku56@gmail.com>
2019-02-18 12:49:10 +0300

    add tests files

--- app/models.py
+++ app/models.py
@@ -1,15 +1,10 @@
 from . import db
 from werkzeug.security import generate_password_hash,check_password_hash
-from flask_login import UserMixin
-from . import login_manager
 from datetime import datetime
 
 
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
 
-class User(UserMixin,db.Model):
+class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(20), index = True)
